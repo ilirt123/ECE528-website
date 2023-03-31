3ebb490cae64cfa623ce72e0985dbc3ed96a422d
ilirt123 <124396897+ilirt123@users.noreply.github.com>
2023-03-31 18:27:47 -0400

    Create data.js

---
+++ data.js
@@ -0,0 +1,25 @@
+{
+    
+  "instances": [
+    {
+      "usmers": 2,
+      "gender": 3,
+      "patient_type": 0,
+      "intubated": 1,
+      "pneumonia": 2,
+      "age": 50,
+      "classification": 3,
+      "medical_type": 0,
+      "diabetes": 0,
+      "copd": 1,
+      "hypertension": 2,
+      "other_disease": 1,
+      "cardiovascular": 0,
+      "obesity": 2,
+      "renal_chronic": 2,
+      "tobacco": 0,
+      "icu": 3
+    }   
+    
+  ]
+}
