055d99d8cf5779344b92484e670df4cda9767e2e
ilirt123 <124396897+ilirt123@users.noreply.github.com>
2023-04-01 14:16:45 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,42 +1,56 @@
-const form = document.getElementById('data-form');
-const instancesDiv = document.getElementById('instances');
+const endpointId = "6567598456985092096";
+const projectId = "my-project-1-ece-528";
+const endpointUrl = `https://us-central1-aiplatform.googleapis.com/v1/projects/${projectId}/locations/us-central1/endpoints/${endpointId}:predict`;
 
-form.addEventListener('submit', async (event) => {
+const form = document.querySelector("#form");
+const resultDiv = document.querySelector("#result");
+
+form.addEventListener("submit", async (event) => {
   event.preventDefault();
-
-  // Get the number of instances from the form
-  const numInstances = parseInt(document.getElementById('num-instances').value, 10);
-
-  // Create an array to hold the instances
-  const instances = [];
-
-  // Loop through each instance and get the input values
-  for (let i = 0; i < numInstances; i++) {
-    const instance = {
-      users: parseInt(prompt(`Enter number of users for instance ${i + 1}:`), 10),
-      gender: prompt(`Enter gender for instance ${i + 1}:`),
-      patient_type: prompt(`Enter patient type for instance ${i + 1}:`),
-      intubated: parseInt(prompt(`Enter intubation status (0 or 1) for instance ${i + 1}:`), 10),
-      pneumonia: parseInt(prompt(`Enter pneumonia status (0 or 1) for instance ${i + 1}:`), 10),
-      age: parseInt(prompt(`Enter age for instance ${i + 1}:`), 10),
-      classification: prompt(`Enter classification for instance ${i + 1}:`),
-    };
-    instances.push(instance);
+  const formData = new FormData(event.target);
+  const inputs = Object.fromEntries(formData.entries());
+  
+  const payload = {
+    "instances": [
+      {
+        "usmers": 2,
+        "gender": inputs.gender,
+        "patient_type": inputs.patient_type,
+        "intubated": inputs.intubated,
+        "pneumonia": inputs.pneumonia,
+        "age": parseInt(inputs.age),
+        "classification": inputs.classification
+      }
+    ]
+  };
+  
+  try {
+    const response = await fetch(endpointUrl, {
+      method: "POST",
+      headers: {
+        "Authorization": `Bearer ${await getAccessToken()}`
+      },
+      body: JSON.stringify(payload)
+    });
+    const prediction = await response.json();
+    const result = prediction.predictions[0].classification;
+    resultDiv.textContent = `The classification is ${result}.`;
+  } catch (error) {
+    console.error(error);
+    resultDiv.textContent = "An error occurred during the prediction.";
   }
-
-  // Send an HTTP request to the RESTful API
-  const response = await fetch('https://us-central1-aiplatform.googleapis.com/v1/projects/my-project-1-ece-528/locations/us-central1/endpoints/6567598456985092096:predict', {
-    method: 'POST',
-    headers: {
-      'Authorization': `Bearer ${await gapi.auth2.getAuthInstance().currentUser.get().getAuthResponse().id_token}`,
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify({ instances }),
-  });
-
-  // Parse the response and display the results on the page
-  const result = await response.json();
-  const resultsDiv = document.createElement('div');
-  resultsDiv.innerHTML = `<pre>${JSON.stringify(result, null, 2)}</pre>`;
-  instancesDiv.appendChild(resultsDiv);
 });
+
+async function getAccessToken() {
+  const response = await fetch("https://us-central1-aiplatform.googleapis.com/v1/projects/-/serviceAccounts/default/token", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({
+      "scope": "https://www.googleapis.com/auth/cloud-platform"
+    })
+  });
+  const { access_token } = await response.json();
+  return access_token;
+}
