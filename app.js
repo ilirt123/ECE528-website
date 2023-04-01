fbb2e33eefb09fe19b943ddbbb97aa7aefa4cd99
ilirt123 <124396897+ilirt123@users.noreply.github.com>
2023-04-01 16:00:38 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,56 +1,69 @@
-const endpointId = "6567598456985092096";
-const projectId = "my-project-1-ece-528";
-const endpointUrl = `https://us-central1-aiplatform.googleapis.com/v1/projects/${projectId}/locations/us-central1/endpoints/${endpointId}:predict`;
+function submitData() {
+  const usmers = document.getElementById("usmers-input").value;
+  const gender = document.getElementById("gender-input").value;
+  const patientType = document.getElementById("patient-type-input").value;
+  const intubated = document.getElementById("intubated-input").value;
+  const pneumonia = document.getElementById("pneumonia-input").value;
+  const age = document.getElementById("age-input").value;
 
-const form = document.querySelector("#form");
-const resultDiv = document.querySelector("#result");
-
-form.addEventListener("submit", async (event) => {
-  event.preventDefault();
-  const formData = new FormData(event.target);
-  const inputs = Object.fromEntries(formData.entries());
-  
-  const payload = {
+  const data = {
     "instances": [
       {
-        "usmers": 2,
-        "gender": inputs.gender,
-        "patient_type": inputs.patient_type,
-        "intubated": inputs.intubated,
-        "pneumonia": inputs.pneumonia,
-        "age": parseInt(inputs.age),
-        "classification": inputs.classification
+        "usmers": usmers,
+        "gender": gender,
+        "patient_type": patientType,
+        "intubated": intubated,
+        "pneumonia": pneumonia,
+        "age": age,
+        "classification": 0
       }
     ]
   };
-  
-  try {
-    const response = await fetch(endpointUrl, {
-      method: "POST",
-      headers: {
-        "Authorization": `Bearer ${await getAccessToken()}`
-      },
-      body: JSON.stringify(payload)
-    });
-    const prediction = await response.json();
-    const result = prediction.predictions[0].classification;
-    resultDiv.textContent = `The classification is ${result}.`;
-  } catch (error) {
-    console.error(error);
-    resultDiv.textContent = "An error occurred during the prediction.";
-  }
-});
 
-async function getAccessToken() {
-  const response = await fetch("https://us-central1-aiplatform.googleapis.com/v1/projects/-/serviceAccounts/default/token", {
+  fetchAccessToken()
+    .then((accessToken) => fetchPrediction(accessToken, data))
+    .catch((error) => console.log(error));
+}
+
+function fetchAccessToken() {
+  const authEndpoint = "https://oauth2.googleapis.com/token";
+  const authParams = {
+    grant_type: "refresh_token",
+    client_id: "764086051850-6qr4p6gpi6hn506pt8ejuq83di341hur.apps.googleusercontent.com",
+    client_secret: "d-FL95Q19q7MQmFpd7hHD0Ty",
+    refresh_token: "1//05BUvbksk_6JmCgYIARAAGAUSNwF-L9IrC-0SoXRqchO9KzfeYtYPpiOn77jvHnhp75YbLRCkH6xybzbaLP4Brqtg0XjcZNxB-_c"
+  };
+
+  return fetch(authEndpoint, {
     method: "POST",
     headers: {
+      "Content-Type": "application/x-www-form-urlencoded"
+    },
+    body: new URLSearchParams(authParams)
+  })
+    .then(response => response.json())
+    .then(data => data.access_token);
+}
+
+function fetchPrediction(accessToken, data) {
+  const endpointId = "6567598456985092096";
+  const projectId = "my-project-1-ece-528";
+  const url = `https://us-central1-aiplatform.googleapis.com/v1/projects/${projectId}/locations/us-central1/endpoints/${endpointId}:predict`;
+
+  fetch(url, {
+    method: "POST",
+    headers: {
+      "Authorization": `Bearer ${accessToken}`,
       "Content-Type": "application/json"
     },
-    body: JSON.stringify({
-      "scope": "https://www.googleapis.com/auth/cloud-platform"
+    body: JSON.stringify(data)
+  })
+    .then(response => response.json())
+    .then(result => {
+      const prediction = result.predictions[0];
+      const output = prediction[0];
+      // do something with the prediction output
     })
-  });
-  const { access_token } = await response.json();
-  return access_token;
+    .catch(error => console.log(error));
 }
+
