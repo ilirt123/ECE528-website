f9287bf882a96c2106cd84d7d7c8b9e983326f22
ilirt123 <124396897+ilirt123@users.noreply.github.com>
2023-04-01 14:00:41 -0400

    Update app.js

--- app.js
+++ app.js
@@ -1,47 +1,42 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const { exec } = require('child_process');
+const form = document.getElementById('data-form');
+const instancesDiv = document.getElementById('instances');
 
-const app = express();
-const port = process.env.PORT || 3000;
+form.addEventListener('submit', async (event) => {
+  event.preventDefault();
 
-app.use(bodyParser.json());
-app.use(express.static('public'));
+  // Get the number of instances from the form
+  const numInstances = parseInt(document.getElementById('num-instances').value, 10);
 
-app.post('/predict', (req, res) => {
-  // Get the input data from the client-side
-  const inputData = req.body.instances;
+  // Create an array to hold the instances
+  const instances = [];
 
-  // Set up the environment variables
-  const endpointId = process.env.ENDPOINT_ID;
-  const projectId = process.env.PROJECT_ID;
-  const inputFilename = 'input.json';
+  // Loop through each instance and get the input values
+  for (let i = 0; i < numInstances; i++) {
+    const instance = {
+      users: parseInt(prompt(`Enter number of users for instance ${i + 1}:`), 10),
+      gender: prompt(`Enter gender for instance ${i + 1}:`),
+      patient_type: prompt(`Enter patient type for instance ${i + 1}:`),
+      intubated: parseInt(prompt(`Enter intubation status (0 or 1) for instance ${i + 1}:`), 10),
+      pneumonia: parseInt(prompt(`Enter pneumonia status (0 or 1) for instance ${i + 1}:`), 10),
+      age: parseInt(prompt(`Enter age for instance ${i + 1}:`), 10),
+      classification: prompt(`Enter classification for instance ${i + 1}:`),
+    };
+    instances.push(instance);
+  }
 
-  // Write the input data to a JSON file
-  const fs = require('fs');
-  fs.writeFileSync(inputFilename, JSON.stringify({ instances: inputData }));
+  // Send an HTTP request to the RESTful API
+  const response = await fetch('https://us-central1-aiplatform.googleapis.com/v1/projects/my-project-1-ece-528/locations/us-central1/endpoints/6567598456985092096:predict', {
+    method: 'POST',
+    headers: {
+      'Authorization': `Bearer ${await gapi.auth2.getAuthInstance().currentUser.get().getAuthResponse().id_token}`,
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify({ instances }),
+  });
 
-  // Execute the prediction request using the gcloud command
-  exec(
-    `curl -X POST -H "Authorization: Bearer $(gcloud auth print-access-token)" \
-    -H "Content-Type: application/json" \
-    https://us-central1-aiplatform.googleapis.com/v1/projects/${projectId}/locations/us-central1/endpoints/${endpointId}:predict \
-    -d "@${inputFilename}"`,
-    (err, stdout, stderr) => {
-      if (err) {
-        console.error(`exec error: ${err}`);
-        return;
-      }
-
-      // Parse the prediction result from the output of the gcloud command
-      const result = JSON.parse(stdout);
-
-      // Send the prediction result back to the client-side
-      res.send(result);
-    }
-  );
-});
-
-app.listen(port, () => {
-  console.log(`Server listening on port ${port}`);
+  // Parse the response and display the results on the page
+  const result = await response.json();
+  const resultsDiv = document.createElement('div');
+  resultsDiv.innerHTML = `<pre>${JSON.stringify(result, null, 2)}</pre>`;
+  instancesDiv.appendChild(resultsDiv);
 });
