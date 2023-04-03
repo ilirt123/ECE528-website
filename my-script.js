3baad1cfdf18d6bf9e4df742018e691ab3c77966
ilirt123 <124396897+ilirt123@users.noreply.github.com>
2023-04-03 16:30:09 -0400

    Update my-script.js

--- my-script.js
+++ my-script.js
@@ -1,19 +1,32 @@
-function predict() {
-  const endpointId = "6567598456985092096";
-  const projectId = "my-project-1-ece-528";
-  const inputDataFile = "path/to/data.json";
-
-  fetch('/predict', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body: JSON.stringify({ endpointId, projectId, inputDataFile })
-  })
-  .then(response => response.json())
-  .then(data => {
-    const predictionElement = document.getElementById('prediction');
-    predictionElement.innerText = data.predicted_result;
-  })
-  .catch(error => console.error(error));
+function UserAction() {
+    var xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function() {
+         if (this.readyState == 4 && this.status == 200) {
+             alert(this.responseText);
+         }
+    };
+    xhttp.open("POST", "https://us-central1-aiplatform.googleapis.com/v1/projects/$6567598456985092096/locations/us-central1/endpoints/$my-project-1-ece-528:predict, true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+    xhttp.send("instances": [
+    {
+      "usmers": 2,
+      "gender": 3,
+      "patient_type": 0,
+      "intubated": 1,
+      "pneumonia": 2,
+      "age": 50,
+      "classification": 3,
+      "medical_type": 0,
+      "diabetes": 0,
+      "copd": 1,
+      "hypertension": 2,
+      "other_disease": 1,
+      "cardiovascular": 0,
+      "obesity": 2,
+      "renal_chronic": 2,
+      "tobacco": 0,
+      "icu": 3
+    }   
+    
+  ]");
 }
