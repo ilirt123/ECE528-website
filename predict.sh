89b6b84fb33c3b711f25b52df22b296b5e516a3d
ilirt123 <124396897+ilirt123@users.noreply.github.com>
2023-04-09 13:39:35 -0400

    Delete predict.sh

--- predict.sh
+++
@@ -1,6 +0,0 @@
-curl \
--X POST \
--H "Authorization: Bearer $(gcloud auth print-access-token)" \
--H "Content-Type: application/json" \
-https://us-central1-aiplatform.googleapis.com/v1/projects/${PROJECT_ID}/locations/us-central1/endpoints/${ENDPOINT_ID}:predict \
--d "@${INPUT_DATA_FILE}"
